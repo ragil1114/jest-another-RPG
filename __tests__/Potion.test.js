c03839dba8370b81db32f15c1e44a3f44e7ddce6
Ragil14 <rahil.gilani@gmail.com>
2022-01-20 17:15:22 -0600

    added test to create a health potion object & another test to create a random potion object to Potion.test.js file

--- __tests__/Potion.test.js
+++ __tests__/Potion.test.js
@@ -1,8 +1,18 @@
+// test to create a health potion object
 const Potion = require('../lib/Potion.js');
 
 test('creates a health potion object', () => {
   const potion = new Potion('health');
 
   expect(potion.name).toBe('health');
+  expect(potion.value).toEqual(expect.any(Number));
+});
+
+// test to create a random potion object
+test('creates a random potion object', () => {
+  const potion = new Potion();
+
+  expect(potion.name).toEqual(expect.any(String));
+  expect(potion.name.length).toBeGreaterThan(0);
   expect(potion.value).toEqual(expect.any(Number));
 });

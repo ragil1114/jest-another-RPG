993d9be0ac3920afa75501db01aff68636500a71
Ragil14 <rahil.gilani@gmail.com>
2022-01-20 18:11:46 -0600

    added test in Player.test.js file to create player object & check for properties using expect() method

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -1,1 +1,11 @@
 const Player = require('../lib/Player');
+
+// test to create a player & check for health, strength & agility properties
+test('creates a player object', () => {
+    const player = new Player('Dave');
+  
+    expect(player.name).toBe('Dave');
+    expect(player.health).toEqual(expect.any(Number));
+    expect(player.strength).toEqual(expect.any(Number));
+    expect(player.agility).toEqual(expect.any(Number));
+});

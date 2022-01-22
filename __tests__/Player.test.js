c502905a1187b945da1f226b90bac404dfd1227f
Ragil14 <rahil.gilani@gmail.com>
2022-01-22 16:06:48 -0600

    added test for getInventory

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -24,3 +24,13 @@ test("gets player's stats as an object", () => {
     expect(player.getStats()).toHaveProperty('strength');
     expect(player.getStats()).toHaveProperty('agility');
 });
+
+test('gets inventory from player or returns false', () => {
+    const player = new Player('Dave');
+  
+    expect(player.getInventory()).toEqual(expect.any(Array));
+  
+    player.inventory = [];
+  
+    expect(player.getInventory()).toEqual(false);
+});
